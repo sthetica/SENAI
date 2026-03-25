e0754a554dedbfdd40e713dc60f47fdd62462a52
sthetica <sthefany_nichelle@estudante.sesisenai.org.br>
2026-03-25 19:04:26 +0000

    update

--- atividades python/tabuada.py
+++ atividades python/tabuada.py
@@ -1,8 +1,16 @@
-while True
+while True:
 
     num1 = int(input("Digite o primeiro número: "))
     num2 = int(input("Digite o segundo número: "))
-    resultado = num1 * num2
+
+    if num1 > 10:
+        print("Seu número é maior que 10, insira um número menor")
+        continue
+    if num1 > 10:
+        print("Seu número é maior que 10, insira um número menor")
+        continue
+
+    resultado = num1 * num2 
     print(f"O reultado da sua multiplicação é: {resultado}")
 
     
